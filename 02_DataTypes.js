06a31b23bb7dbb5f826cee2a1fd221cb3b8c3c60
Nizam <nizamuddin.razza@gmail.com>
2025-06-27 12:27:02 +0530

    initial commit

---
+++ 02_DataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all js code as newer version
+
+// alert("hello") we are using nodejs, not browser
+
+console.log(3+3)
+
+let name ="nizam"
+let age = 21
+let isLoggedIn = true
+
+// primitive data types
+
+//number = 2 to power 53
+//bigint
+//string = ""
+//boolean = true/false
+//null = standAlone value (object type)
+//undefined = value is not assigned
+//symbol = define unique
