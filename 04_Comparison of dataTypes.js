85537d555870ca9a898058964cce93dbff0a1442
Nizam <nizamuddin.razza@gmail.com>
2025-06-29 00:32:08 +0530

    initial commit

---
+++ 04_Comparison of dataTypes.js
@@ -0,0 +1,26 @@
+/*
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 <= 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+
+console.log("2" > 1 );
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+
+comparisions shown above we should avoid
+Reason - if the user filled wrong data type but js will take the input even if it is wrong conversion
+*/
+
+// strict check
+
+console.log("2" === 2);
