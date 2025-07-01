7fc6a5d0aa5e199d10e89380745cb6cf1a79dcfe
Nizam <nizamuddin.razza@gmail.com>
2025-07-01 09:18:54 +0530

    initial commit

---
+++ 06_String in Js.js
@@ -0,0 +1,26 @@
+const name = "Nizam"
+const repoCount = 50
+
+console.log(name + repoCount);
+
+// modern type of script writting
+
+console.log(`${name} ${repoCount}` );
+
+const gameName = new String('NizamFT')
+
+console.log(gameName[0]);
+console.log(gameName.toUpperCase());
+
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const newStringOne = "     Jamun    "
+console.log(newStringOne.trim());
+
+const url = "www.google.com+apple"
+console.log(url.replace('+', '/'));
+
+
+
